82421d982a6d1f5cd1e5db7ad285ae60a9569a49
ibrahim75 <ibrahimbiplob75@gmail.com>
2023-11-30 20:32:21 +0600

    Sheef Recomenrd

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import CarouselSlider from "./CarouselSlider";
 import Featured from "./Featured";
 import PopularMenu from "./PopularMenu";
+import Recomend from "./Recomend";
 import Swipper from "./Swipper";
 import Testimonials from "./Testimonials"
 
@@ -10,14 +11,21 @@ import Testimonials from "./Testimonials"
 
 const Home = () => {
     return (
-        <div>
-            <CarouselSlider></CarouselSlider>
-            <Swipper></Swipper>
-            <PopularMenu></PopularMenu>
-            <Featured></Featured>
-             <Testimonials></Testimonials>
-            
+      <div>
+        <CarouselSlider></CarouselSlider>
+        <Swipper></Swipper>
+        <PopularMenu></PopularMenu>
+        <div className="text-center bg-black w-3/4 h-40 m-auto justify-center">
+          <div className="pt-6">
+            <h3 className="text-white mt-8 text-5xl text-center">
+              Call Us: +880 1571286724
+            </h3>
+          </div>
         </div>
+        <Recomend></Recomend>
+        <Featured></Featured>
+        <Testimonials></Testimonials>
+      </div>
     );
 };
 
--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -3,8 +3,8 @@
 const Footer = () => {
     return (
       <div className="">
-        <footer className="footer p-10 bg-neutral text-neutral-content">
-          <aside>
+        <footer className="footer  bg-neutral text-neutral-content">
+          <aside className="bg-[#1F2937] w-full p-10">
             <svg
               width="50"
               height="50"
@@ -22,7 +22,7 @@ const Footer = () => {
               Providing reliable tech since 1992
             </p>
           </aside>
-          <nav>
+          <nav className="  w-full p-10">
             <header className="footer-title">Social</header>
             <div className="grid grid-flow-col gap-4">
               <a>
@@ -61,9 +61,9 @@ const Footer = () => {
             </div>
           </nav>
         </footer>
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <footer className="footer bg-[#151515] footer-center p-4 text-white  text-base-content">
           <aside>
-            <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+            <p>Copyright © CulinaryCloud. All rights reserved.</p>
           </aside>
         </footer>
       </div>
